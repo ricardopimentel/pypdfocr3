5e51ba085d5fbef33d3d609f487d1f0c72569972
ricardopimentel <ricardoppimentel@gmail.com>
2017-07-04 14:00:41 -0300

    Update pypdfocr_tesseract.py

--- pypdfocr/pypdfocr_tesseract.py
+++ pypdfocr/pypdfocr_tesseract.py
@@ -162,7 +162,7 @@ class PyTesseract(object):
         logging.info("Running OCR on %s to create %s.html" % (img_filename, basename))
         #cmd = '%s "%s" "%s" -psm 1 -c hocr_font_info=1 -l %s hocr' % (self.binary, img_filename, basename, self.lang)
         #Sintaxe correta para pegar o idioma indicano na linha de comando
-        cmd = '%s "%s" "%s" -l %s -psm 1 -c hocr_font_info=1 -l hocr' % (self.binary, img_filename, basename, self.lang)
+        cmd = '%s "%s" "%s" -l %s -psm 1 -c hocr_font_info=1 hocr' % (self.binary, img_filename, basename, self.lang)
         logging.info(cmd)
         try:
             ret_output = subprocess.check_output(cmd, shell=True,  stderr=subprocess.STDOUT)
